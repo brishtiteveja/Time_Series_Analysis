44e878fe1c5c1f3b99e3ce7bd56ada3a4280e40c
Zehady Abdullah Khan <brishtiteveja@gmail.com>
2018-06-20 14:16:53 -0400

    Holocene epoch human culture column data extraction code re-written in R.

--- Holocene_Epoch_Spectrum_Analysis.R
+++ Holocene_Epoch_Spectrum_Analysis.R
@@ -1,18 +1,382 @@
 ## Spectrum Analysis
 #Demean and Detrend data
 #```{r}
+library(readxl)
+library(plotly)
+library(dplyr)
+
+# project directory
+proj_dir <- '/Users/andy/Dropbox/TSCreator/TSCreator development/Developers/Andy/Projects/ML-Data Mining/CultureClimate/'
+# datapack directory
+dp_dir <- '/Users/andy/Dropbox/TSCreator/TSCreator development/Developers/Andy/Projects/ML-Data Mining/datapacks/'
+setwd(dp_dir)
+dp_fname <- 'HumanCulture MidEastIntervals wGreenland-Ice cleaned 19Jan2016.xls'
+
+dfxl <- read_excel(dp_fname)
+c <- colnames(dfxl) # column header
+df2c <- dfxl[[c[2]]] # second column
+
+STARTING_AGE <- 0 + 0.000001
+ENDING_AGE <- 12005/1000 # 2Ka
+AGE_SLIDE <- 50/1000 # 50 yr
+
+# second column
+c <- list()
+c <- colnames(dfxl)
+df2c <- dfxl[[c[2]]]
+
+# Check column formats on : https://engineering.purdue.edu/Stratigraphy/tscreator/download/TSC_ColumnFormats_Examples_Sept2017.txt
+column_type <- c('block', 'event', 'point')
+
+columns <- list()
+for (ct in column_type) {
+  msg <- paste("Processing ", ct, " column.\n",
+             "----------------------------------------------------------------------",
+               sep="")
+  print(msg)
+  columns[[ct]] <- list() 
+  
+  # get excel row num for each column
+  rows <- which(df2c == ct) + 1
+  
+  
+  cols <- list()
+  if (ct == 'block') {
+    cnum <- 1
+    for (r in rows) {
+      cols[[cnum]] <- list(name = c(), content=list())
+      colname <- dfxl[[c[1]]][r-1]
+      cols[[cnum]]$name <- colname
+
+      r_i <- r
+      column <- list()
+      while(TRUE) {
+        age <- dfxl[[c[3]]][r_i]
+        if (is.na(age)) {
+          break
+        }
+        name <- dfxl[[c[2]]][r_i]
+        if (is.na(name) && !is.na(age))
+          name <- ""
+        line_type <- dfxl[[c[4]]][r_i]
+        popup <- dfxl[[c[5]]][r_i]
+
+        column$name <- c(column$name, name)
+        column$age <- c(column$age, as.numeric(age))
+        column$line_type <- c(column$line_type, line_type)
+        column$popup <- c(column$popup, popup)
+        r_i <- r_i + 1
+      }
+      cols[[cnum]]$content <- column
+      cnum <- cnum + 1
+    }
+  }
+  else if (ct == 'event') {
+    cnum <- 1
+    for (r in rows) {
+      cols[[cnum]] <- list(name = c(), content=list())
+      colname <- dfxl[[c[1]]][r-1]
+      cols[[cnum]]$name <- colname
+      
+      r_i <- r
+      column <- list()
+      type <- NA
+      while(TRUE) {
+        age <- dfxl[[c[3]]][r_i]
+        if (is.na(age)) {
+          if (is.na(dfxl[[c[1]]][r_i]))
+            break
+          else {
+            type <- dfxl[[c[1]]][r_i]
+            r_i <- r_i + 1
+            next
+          }
+        }
+        name <- dfxl[[c[2]]][r_i]
+        if (is.na(name) && !is.na(age))
+          name <- ""
+        line_type <- dfxl[[c[4]]][r_i]
+        popup <- dfxl[[c[5]]][r_i]
+        
+        column$name <- c(column$name, name)
+        column$age <- c(column$age, as.numeric(age))
+        if (!is.na(type))
+          column$type <- c(column$type, type)
+        column$line_type <- c(column$line_type, line_type)
+        column$popup <- c(column$popup, popup)
+        r_i <- r_i + 1
+      }
+      cols[[cnum]]$content <- column
+      cnum <- cnum + 1
+    }
+  } 
+  else if (ct == 'point') {
+    cnum <- 1
+    for (r in rows) {
+      cols[[cnum]] <- list(name = c(), content=list())
+      colname <- dfxl[[c[1]]][r-1]
+      cols[[cnum]]$name <- colname
+
+      r_i <- r + 1
+      column <- list()
+      while(TRUE) {
+        age <- dfxl[[c[2]]][r_i]
+        if (is.na(age)) {
+          break
+        }
+        name <- dfxl[[c[3]]][r_i]
+        popup <- dfxl[[c[5]]][r_i]
+
+        column$age <- c(column$age, as.numeric(age))
+        column$name <- c(column$name, name) # actually value
+        column$popup <- c(column$popup, popup)
+        r_i <- r_i + 1
+      }
+      cols[[cnum]]$content <- column
+      cnum <- cnum + 1
+    }
+  }
+  columns[[ct]] <- cols
+}
+
+regional_column_list <- list()
+
+region_info_rows <- which(dfxl[[c[2]]] == ':')
+
+for (r in region_info_rows) {
+  region <- dfxl[[c[1]]][r]
+  if (region %in% regional_columns) {
+    regional_column_list[[region]] <- list()
+  } else {
+    next
+  }
+  sub_regions <- list()
+  for (rc in c[3:12]) {
+    sub_region_n <- dfxl[[rc]][r]
+    if(!is.null(sub_region_n) && !is.na(sub_region_n) && 
+       !not_column(sub_region_n)) {
+      sub_regions[[sub_region_n]] <- list()
+      
+      # Get sub sub region row number
+      sr <- which(dfxl[[c[1]]] == sub_region_n)
+      sub_sub_regions <- list()
+      for (src in c[3:12]) {
+        sub_sub_region_n <- dfxl[[src]][sr]
+        if(!is.null(sub_sub_region_n) && !is.na(sub_sub_region_n) &&
+           !not_column(sub_sub_region_n)) {
+          sub_sub_regions <- c(sub_sub_regions, sub_sub_region_n) 
+        }
+      }
+      sub_regions[[sub_region_n]] <- sub_sub_regions
+    }
+  }
+  regional_column_list[[region]] <- sub_regions
+}
+
+get_main_region_name <- function(col_name) {
+  if (col_name %in% regional_columns) {
+    return(col_name)
+  } else {
+    for(region in regional_columns) {
+      # number of sub-regions
+      sub_region_num <- length(regional_column_list[[region]])
+      if (sub_region_num == 0)
+        next
+      sub_regions <- names(regional_column_list[[region]][1:sub_region_num])
+      if (col_name %in% sub_regions)
+        return(region)
+      else {
+        for(sub_region_n in sub_regions) {
+          sub_region <- regional_column_list[[region]][[sub_region_n]]
+          sub_sub_regions <- unlist(sub_region)
+          sub_sub_region_num <- length(sub_sub_regions)
+          if (sub_sub_region_num == 0)
+            next
+          if (col_name %in% sub_sub_regions)
+            return(region)
+        }
+      }
+    }
+  }
+  
+  return(NA)
+}
+
+# Get column wise event numbers
+dir <- proj_dir 
+
+events_by_col <- list()
+event_names_by_col <- list()
+ev_df_by_col <- list()
+for(col_type in column_type) {
+  msg <- paste("Processing ", col_type, " column events.\n",
+               "----------------------------------------------------------------------",
+               sep="")
+  print(msg)
+  ncol <- length(columns[[col_type]])
+  
+  msg <-paste("Total number of " , col_type , " columns = ", ncol, sep ="") 
+  print(msg)
+  
+  if (ncol == 0) {
+    msg <- paste("No column data or not extracted yet.")
+    next
+  }
+  
+  start_age <- STARTING_AGE
+  end_age <- ENDING_AGE
+  age_diff <- AGE_SLIDE
+  
+  events <- list()
+  event_names <- list()
+  event_types <- list()
+  
+  while(start_age <= end_age) {
+    next_age <- start_age + age_diff
+    mid_age <- (start_age + next_age) / 2
+    
+    key <- as.character(mid_age)
+    evs <- 0
+    ev_names <- c()
+    ev_types <- c()
+    
+    for(col in columns[[col_type]]){
+      cn <- length(col$content$age)
+      for (k in 1:cn) {
+        if (length(col$content) == 0)
+          next
+        a <- col$content$age[k]
+        n <- col$content$name[k]
+        t <- col$content$type[k]
+        if (a >= start_age && a < next_age) {
+          evs <- as.numeric(evs) + 1
+          ev_names <- c(ev_names, n)
+          ev_types <- c(ev_types, t)
+        }
+        k <- k+1
+      }
+    }
+    events[[key]] <- evs
+    event_names[[key]] <- ev_names
+    if(col_type == 'event')
+      event_types[[key]] <- ev_types
+    start_age <- next_age
+  }
+  
+  age <- as.numeric(names(events))
+  freq <- as.numeric(unlist(events))
+  ev_df <- data.frame(age = age, freq = freq)
+ 
+  msg <- paste("Saving age vs frequency plot.")
+  print(msg)
+  im_str <- paste(col_type, '_column_', AGE_SLIDE, '_mil_event_frequency_through_phanerozoic', sep="")
+  image_fn <- paste(dir, 'images/', im_str, ".png", sep="")
+  png(image_fn)
+  plot(age, freq, main=im_str, t='l')
+  dev.off()
+  
+  p <- plot_ly(data=ev_df, 
+               x=~age, y=~freq, 
+               type='scatter',
+               mode='lines',
+               line=list(color='black')) %>%
+       layout(title=im_str)
+  print(p)
+  
+  data_dir <- paste(dir, 'data/', sep="")
+  event_names_fn <- paste(col_type, '_column_', AGE_SLIDE, '_mil_event_names_phanerozoic.txt', sep="")
+  fn0 <- paste(data_dir, event_names_fn, sep="")
+  msg <- paste("Writing event names in ", fn0, sep="")
+  print(msg)
+  
+  sink(fn0)
+  print(event_names)
+  sink()
+  
+  event_freq_datapack_file_name <- paste('Phanerozoic_', col_type, '_column_every_', AGE_SLIDE,'_mil_event_frequencies_datapack.txt', sep="")
+  fn <- paste(data_dir, event_freq_datapack_file_name, sep="")
+  msg <- paste("Writing datapack ", fn, sep="")
+  print(msg)
+  
+  sink(fn)
+  str <- 'format version:	1.3\nage units:	Ma\n'
+  writeLines(str)
+  str <- paste(col_type, ' Column ', AGE_SLIDE, ' Million Year Event Frequencies\tpoint\t150\t255/245/230', sep="")
+  writeLines(str)
+  nf <- length(freq)
+  for (l in 1:nf) {
+    a <- age[l]
+    f <- freq[l]
+    ln <- paste("\t", a, "\t", f, sep='')
+    writeLines(ln)
+  }
+  sink()
+  #file.show(fn)
+  
+  event_names_by_col[[col_type]] <- event_names
+  events_by_col[[col_type]] <- events
+  ev_df_by_col[[col_type]] <- ev_df
+}
+
+# Regional Columns
+regional_columns <- c('Africa', 'Eastern Mediterranean', 'Middle East to India',
+                      'East Asia and Oceania', 'Europe', 'Arctic and Subarctic',
+                      'Northwest and Canada', 'North America', 'Middle America',
+                      'South America')
+
+not_column <- function(col_name) {
+  if (col_name == '_METACOLUMN_OFF')
+    return(TRUE)
+  else if (col_name == 'off' || col_name == 'on') {
+    return(TRUE)
+  } else {
+    # # column width
+    m <- regexec("^[0-9]+$",col_name)
+    ml <- regmatches(col_name, m)
+    if (length(ml[[1]]) == 1) {
+      return(TRUE)
+    }
+
+    # color code
+    m <- regexec("([0-9])+/([0-9])+/([0-9])+",col_name)
+    ml <- regmatches(col_name, m)
+    
+    if (length(ml[[1]]) != 0) {
+      return(TRUE)  
+    }
+    
+    return(FALSE)
+  }
+}
 
 
 # Number of events every 50 year
-data_dir <- ('/Users/andy/Dropbox/TSCreator/TSCreator development/Developers/Andy/Projects/ML-Data Mining/programming/')
-fn_50 <- paste(data_dir, 'event_frequency_per_50_yr_dat.txt', sep='')
-ev_f_50 <- read.csv(fn_50, sep=' ')
+#data_dir <- ('/Users/andy/Dropbox/TSCreator/TSCreator development/Developers/Andy/Projects/ML-Data Mining/programming/')
+#setwd(data_dir)
+#fn_50 <- paste(data_dir, 'event_frequency_per_50_yr_dat.txt', sep='')
+#ev_f_50 <- read.csv(fn_50, sep=' ')
+a <- ev_df_by_col[['block']]$age
+f <- ev_df_by_col[['block']]$freq + ev_df_by_col[['event']]$freq
+ev_f <- data.frame(ages = a, events = f)
 
-time_interval <- 2.25
-ev_f_50 <- subset(ev_f_50, ev_f_50$ages_50 <= time_interval)
+#time_interval <- 2.0
+#ev_f <- subset(ev_f, ev_f$ages <= time_interval)
 
-eva <- ev_f_50$ages_50
-ev <- ev_f_50$events_50
+eva <- ev_f$ages
+ev <- ev_f$events
+# plot
+fn <- paste('Global_event_numbers_',
+        AGE_SLIDE, '_year_bin_from_', round(STARTING_AGE), 
+        '_to_', ENDING_AGE, '_ka', sep="")
+im_fn <- paste(proj_dir, 'images/', fn, '.png', sep="")
+png(im_fn)
+plot(eva, ev, xlab='Age(Ka)', 
+     ylab='Number of Events', type='p', cex=0.25, col='black', lwd=2,
+     main='Number of global events per 50 years', ylim=c(-10,40)
+)
+lines(eva, ev, col='black')
+dev.off()
+
 # demean event data
 ev.dm <- ev - mean(ev)
 
